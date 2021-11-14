7b154ac0689bff36cf7503dec711c2cc6be7ffcd
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-11-14 16:00:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a=1, b=1, c, n, i=1;
+int main()
+{
+    int n,i=1,b=1,a=0,c;
     cin >> n;
 
-        if (n>=2) {
-            cout << b << endl;
-            for (i = 2; i <= n; i++) {
-                c = a + b;
-                cout << c << endl;
-                a = b;
-                b = c;
-            }
-        }else{
-               cout << "errore" << endl;
-        }
-    return 0;
+    if (n>=2)
+    {
+         cout << b << endl;
+         for (i = 2; i <= n; i++) 
+         {
+         c=a+b;
+         cout << c << endl;
+         a=b;
+         b=c;
+             
+         }
+    
+     }
+    
+    else
+     {
+        cout << "errore" << endl;
+     }
+    
+     return 0;
 }
